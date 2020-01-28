4affb05bae05a36c0e67c13a02d92864d1b20d17
marcpeternell <peternellmarc@gmail.com>
2020-01-28 18:11:26 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+//Custom Addons
+import * as BABYLON from "babylonjs";
+//Use GUI
+import * as GUI from "babylonjs-gui";
+//Loader to use glTF Files
+import "babylonjs-loaders";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
